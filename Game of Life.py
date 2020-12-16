bceb076bd06c458a2cc39355e1854ce366a8c648
Alex <AlexStiles123@gmail.com>
2020-12-16 15:31:01 +1300

    starting to seperate main class

--- Game of Life.py
+++ Game of Life.py
@@ -1,9 +1,8 @@
 import console
 import time
 
-CHA_DED = "▫️"
-CHA_ALI = "◾️"
 class InfiniteUniverse:
+
     def __init__(self,height,width):
         self.height = height
         self.width = width
@@ -11,22 +10,22 @@ class InfiniteUniverse:
         self.state = []
         for y in range(self.height):
             self.state.append(list(self.width*CHA_ALI))
-        
+
     def __str__(self):
         output = ""
         for y in self.state:
             output += "".join(y) + "\n"
         return output
-    
+
     def make_alive(self, position):
         self.state[position[1]][position[0]] = CHA_ALI
-    
+
     def create_empty_board(self):
         new_board = []
         for y in range(self.height):
             new_board.append(list(self.width*CHA_DED))
         return new_board
-            
+ 
     def check_neighbours(self,position):
         characters = []
         for y in range(position[1]-1,position[1]+2):
@@ -47,12 +46,11 @@ class InfiniteUniverse:
             if cell == CHA_ALI:
                 alive += 1 
         return (alive,dead)
-        
+
     def is_alive(self,position):
         out = self.state[position[1]][position[0]]== CHA_ALI
         return out
-        
-    
+
     def step(self):
         next_frame = self.create_empty_board()
         for y in range(self.height):
@@ -67,53 +65,31 @@ class InfiniteUniverse:
                 else:
                     next_frame[y][x] = CHA_DED
         self.state = next_frame
-        
-                    
-                
-                
-        
+
+
+class Cell:
+
+    CHA_DED = "▫️"
+    CHA_ALI = "◾️"
+
+    def __init__(self, is_alive : bool) -> bool:
+        self.is_alive = is_alive
+        self.symbol = self.set_symbol()
     
-        
-        
-         
+    def set_symbol(self) {
+        return CHA_ALI if self.is_alive else CHA_DED 
+    }
 
 
-
-a = InfiniteUniverse(20,20)
-#a.make_alive((0+5,0+5))
-#a.make_alive((2+5,0+5))
-#a.make_alive((1+5,1+5))
-#a.make_alive((2+5,1+5))
-#a.make_alive((1+5,2+5))
-
-#a.make_alive((1,1))
-#a.make_alive((2,2))
-#a.make_alive((1,2))
-#a.make_alive((2,1))
-'''
-a.make_alive((6,5))
-a.make_alive((6,6))
-a.make_alive((6,8))
-a.make_alive((6,9))
-a.make_alive((6,10))
-a.make_alive((6,11))
-a.make_alive((6,13))
-a.make_alive((6,14))
-a.make_alive((5,12))
-a.make_alive((7,12))
-a.make_alive((5,7))
-a.make_alive((7,7))
-
-'''
-console.clear()
-while 1==1:
-
-    
-    print(a)
-    a.step()
-    time.sleep(0.5)
+def main():
     console.clear()
-    
- 
+    a = InfiniteUniverse(20,20)
 
+    while True:
+        print(a)
+        a.step()
+        time.sleep(0.5)
+        console.clear()
 
+if __name__ == "__main__":
+    main()
